493e6e1ec824ce2debe0c3a079b38b82cd1050a7
MoonSangWon <kuim75@naver.com>
2022-10-25 16:16:16 +0900

    feat: WebConfiguration.class cors add

--- src/main/java/SpringBoot/Codebase/config/WebConfiguration.java
+++ src/main/java/SpringBoot/Codebase/config/WebConfiguration.java
@@ -1,2 +1,17 @@
-package SpringBoot.Codebase.config;public class WebConfiguration {
+package SpringBoot.Codebase.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET","POST");
+    }
+
 }
