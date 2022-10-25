c761e17b99716c84dd3a4109fa29833c0627a41b
MoonSangWon <kuim75@naver.com>
2022-10-25 18:15:55 +0900

    fix: DB & measurement name 수정

---
+++ src/main/java/SpringBoot/Codebase/controller/PubSensorController.java
@@ -0,0 +1,2 @@
+package SpringBoot.Codebase.controller;public class PubSensorController {
+}
--- src/main/java/SpringBoot/Codebase/domain/dto/Sensordto.java
+++ src/main/java/SpringBoot/Codebase/domain/dto/Sensordto.java
@@ -5,56 +5,58 @@ import org.influxdb.annotation.Measurement;
 import org.influxdb.annotation.TimeColumn;
 import org.springframework.beans.factory.annotation.Value;
 
-@Measurement(name = "test_db")
+@Measurement(name = "smartfarm_db")
 public class Sensordto {
 
     @TimeColumn
-    @Column(name ="humidity")
+    @Column(name = "humidity")
     private Double humidity;
     @Column(name = "temperature")
     private Double temperature;
     @Column(name = "cdc")
     private Double cdc;
+    @Column(name = "soil")
+    private Double soil;
     @Column(name = "farmname")
     private String farmName;
 
-    public Double geTemperature() {
-        return temperature;
-    }
-    public void setTemperature(Double temperature) {
-        this.temperature = temperature;
-    }
-    public Double getMoisture() {
-        return moisture;
-    }
-    public void setMoisture(Double password) {
-        this.moisture = moisture;
-    }
-
-    public void setHuminity(Double huminity){
-        this.huminity = huminity;
-    }
-    public Double getHuminity(Double huminity){
-        return huminity;
-    }
-
-    public String getFarmname() {
-        return farmName;
-    }
-    public void setFarmName(String farmName) {
-        this.farmName = farmName;
-    }
-
-    @Override
-    public String toString() {
-        return "TestMeasurement {" +
-                "farmName=" + farmName +
-                ", Temperature=" + temperature +
-                ", Huminity=" + huminity +
-                ", Moisture=" + moisture +
-                ",Temperature=" + temperature +
-
-                '}';
-    }
-
+//    public Double geTemperature() {
+//        return temperature;
+//    }
+//    public void setTemperature(Double temperature) {
+//        this.temperature = temperature;
+//    }
+//  //  public Double getMoisture() {
+//        return moisture;
+//    }
+//  //  public void setMoisture(Double password) {
+//        this.moisture = moisture;
+//    }
+//
+//    public void setHuminity(Double huminity){
+//        this.huminity = huminity;
+//    }
+//    public Double getHuminity(Double huminity){
+//        return huminity;
+//    }
+//
+//    public String getFarmname() {
+//        return farmName;
+//    }
+//    public void setFarmName(String farmName) {
+//        this.farmName = farmName;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "TestMeasurement {" +
+//                "farmName=" + farmName +
+//                ", Temperature=" + temperature +
+//                ", Huminity=" + huminity +
+//                ", Moisture=" + moisture +
+//                ",Temperature=" + temperature +
+//
+//                '}';
+//    }
+//
 }
