3568dfc96909634de7850d098d17bac99adb0bfe
Hoon9901 <shonn.dev@gmail.com>
2022-11-17 21:52:45 +0900

    feat JVM timezone KST

--- src/main/java/SpringBoot/Codebase/CodebaseApplication.java
+++ src/main/java/SpringBoot/Codebase/CodebaseApplication.java
@@ -3,8 +3,18 @@ package SpringBoot.Codebase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class CodebaseApplication {
+
+	@PostConstruct
+	public void started() {
+		// timezone UTC 셋팅
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodebaseApplication.class, args);
